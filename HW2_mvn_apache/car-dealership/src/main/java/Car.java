e2ceeaf0457c3211b34a1009b0f42c5b34533a8e
VolodymyrDeveloper <xalimonenko@gmail.com>
2022-01-05 01:03:06 +0200

    1.Rename the class 2. Add "private" access modifiers for all fields 3. Remove getters and setters and toString ()

---
+++ HW2_mvn_apache/car-dealership/src/main/java/Car.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+
+@Data
+public class Car {
+    private String name;
+
+    private String color;
+
+    private int maxSpeed;
+
+
+}
--- HW2_mvn_apache/central-sales-office/src/main/java/CentralOffice.java
+++ HW2_mvn_apache/central-sales-office/src/main/java/CentralOffice.java
@@ -1,7 +1,8 @@
 public class CentralOffice {
     public static void main(String[] args) {
 
-        Nissan car = new Nissan();
+        Car car = new Car();
+        car.setName("Nissan");
         car.setColor("red");
         car.setMaxSpeed(200);
         System.out.println("Info about car: " + car);
